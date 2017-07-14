cc5beb7aea084d50abe6360a1e5302968d8a5971
Joe Kramer <joekramer1125@gmail.com>
2017-07-14 07:33:39 -0700

    fix names showing up and initialized with id

--- src/main/java/Member.java
+++ src/main/java/Member.java
@@ -1,8 +1,15 @@
+import java.util.List;
+import java.util.ArrayList;
+
 public class Member {
   private String mName;
+  private int mId;
+  private static List<Member> instances = new ArrayList<Member>();
 
   public Member(String name) {
     mName = name;
+    instances.add(this);
+    mId = instances.size();
   }
 
   public String getName() {
